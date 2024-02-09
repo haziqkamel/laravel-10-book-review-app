0718efc9e24cd52c5961e5adcdad55ead57e0a8e
Haziq Kamel <haziqkamel@outlook.com>
2024-02-09 16:51:07 +0800

    add more scope in Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -25,14 +25,33 @@ class Book extends Model
 
     // Popular Scope
     // Get the most popular book with most reviews count
-    public function scopePopular(Builder $query): Builder
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder
     {
-        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+        return $query->withCount(['reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)])
+            ->orderBy('reviews_count', 'desc');
     }
 
     // Highest Rated Scope
-    public function scopeHighestRated(Builder $query): Builder
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder
     {
-        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+        return $query->withAvg(['reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    // Min Amount of Reviews
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
