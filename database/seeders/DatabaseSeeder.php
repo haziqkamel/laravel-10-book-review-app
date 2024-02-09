99f415059bd70b52628c53d810cf2cacfa6cfbd5
Haziq Kamel <haziqkamel@outlook.com>
2024-02-09 15:10:21 +0800

    db: data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,17 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Book::factory(33)->create()->each(function (Book $book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)->average()->for($book)->create();
+        });
+
+        Book::factory(34)->create()->each(function (Book $book) {
+            $numReviews = random_int(5, 30);
+
+            Review::factory()->count($numReviews)->bad()->for($book)->create();
+        });
     }
 }
